4c46c23a73710c0c3c234e7c621c4800b359378c
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-09-16 17:34:51 +0530

    tpot

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,59 +1,35 @@
 canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d");
-const FOREGROUND_COLOR = "#77dd77"
+
+const FOREGROUND_COLOR = "#7dc2b3"
 const BACKGROUND_COLOR = "black"
-const h = 300;
-const w = 300;
-canvas.height = h;
-canvas.width = w;
+
+
+canvas.height = 800;
+canvas.width = 800;
 
 function clear() {
   ctx.fillStyle = BACKGROUND_COLOR;
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-function draw_point({ x, y }, size) {
-  ctx.fillStyle = FOREGROUND_COLOR;
-  ctx.fillRect(x - (size / 2), y - (size / 2), size, size)
-}
-function screen_coords({ x, y }) {
+
+
+function normalized_to_screen_space({ x, y }) {
   // (-1 , 1) + 1 --> (0, 2) /2 --> (0, 1) * w ----> (0, w )
   x = ((x + 1) / 2) * canvas.width;
 
   // (-1 , 1) + 1 --> (0, 2) /2 --> (0, 1) * h ----> (0, h )
   y = ((((y * (-1)) + 1) / 2)) * canvas.height;
   return { x: x, y: y }
-
 }
 
 clear();
-let p = { x: .9, y: .9, z: 1 };
 
 function project({ x, y, z }) {
   return { x: x / z, y: y / z }
-
 }
-const vertices = [
-  { x: .5, y: .5, z: .5 },
-  { x: .5, y: -.5, z: .5 },
-  { x: -.5, y: -.5, z: .5 },
-  { x: -.5, y: .5, z: .5 },
 
-  { x: .5, y: .5, z: -.5 },
-  { x: .5, y: -.5, z: -.5 },
-  { x: -.5, y: -.5, z: -.5 },
-  { x: -.5, y: .5, z: -.5 }
-]
-const faces = [
-  [0, 1, 2, 3],
-  [4, 5, 6, 7],
-  [0, 4], 
-  [1, 5], 
-  [2, 6], 
-  [3, 7], 
-
-
-]
 function rotate_xz({ x, y, z }, theta) {
   // rotate around the y axis
   const new_x = x * Math.cos(theta) - z * Math.sin(theta);
@@ -61,34 +37,72 @@ function rotate_xz({ x, y, z }, theta) {
 
   return { x: new_x, y: y, z: new_z };
 }
-function translate_z({ x, y, z }, dz) {
-  return { x: x, y: y, z: z + dz };
 
+function translate({ x, y, z }, { dx, dy, dz }) {
+  return { x: x + dx, y: y + dy, z: z + dz };
 }
+
+function draw_point({ x, y }, size) {
+  ctx.fillStyle = FOREGROUND_COLOR;
+  ctx.fillRect(x - (size / 2), y - (size / 2), size, size)
+}
+
 function draw_line(p1, p2) {
   ctx.beginPath();
   ctx.strokeStyle = FOREGROUND_COLOR;
-  ctx.lineWidth = 1;
+  ctx.lineWidth = .1;
   ctx.moveTo(p1.x, p1.y)
   ctx.lineTo(p2.x, p2.y)
   ctx.stroke();
 }
+
+function draw_triangle(p1, p2, p3) {
+  draw_line(p1, p2);
+  draw_line(p3, p2);
+  draw_line(p3, p1);
+}
+
 const FPS = 60;
 let d_theta = 0
+let triangles = [];
+function cook_vertex(vertex, d_theta) {
+  let rotated = rotate_xz(vertex, d_theta);
+  let translated = translate(rotated, { dx: 0, dy: -1, dz: 5 });
+  let projected = project(translated);
+  let screen_point = normalized_to_screen_space(projected);
+  return screen_point;
+}
 function frame() {
-
   clear()
-  d_theta += (2 * Math.PI) / (FPS * 4);
-  for (face of faces) {
-    for (let i = 0; i < face.length; i++) {
-      const a = vertices[face[i]];
-      const b = vertices[face[(i + 1) % face.length]];
-      draw_line(
-        screen_coords(project(translate_z(rotate_xz(a, d_theta), 2))),
-        screen_coords(project(translate_z(rotate_xz(b, d_theta), 2)))
-      );
-    }
+  d_theta += (2 * Math.PI) / (FPS * 10);
+  for (const triangle of triangles) {
+    draw_triangle(cook_vertex(triangle[0], d_theta), cook_vertex(triangle[1], d_theta), cook_vertex(triangle[2], d_theta));
+
   }
+
   setTimeout(frame, 1000 / FPS);
 }
 frame();
+let text;
+async function loadData() {
+  try {
+    const response = await fetch('js/data.txt');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    text = await response.text();
+    text = text.split("\n\n");
+    vertices = []
+    for (triangle_text of text) {
+      let points = triangle_text.split("\n");
+      const triangle = points.map(str => {
+        const [x, y, z] = str.trim().split(/\s+/).map(Number);
+        return { x, y, z };
+      });
+      triangles.push(triangle);
+    }
+  } catch (error) {
+    console.error('Failed to load js/data.txt:', error);
+  }
+}
+loadData()
